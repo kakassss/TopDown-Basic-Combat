45f9fb6aae7062181fff88e3ff33b536992c259a
Onur Çelik <celikonur988@gmail.com>
2024-02-22 23:07:05 +0300

    comment lines added to attackState

--- Assets/Scripts/States/PlayerAttackState.cs
+++ Assets/Scripts/States/PlayerAttackState.cs
@@ -5,18 +5,20 @@ using UnityEngine.InputSystem;
 public class PlayerAttackState : PlayerBaseState
 {
     private List<float> attackAnimationsDurations;
+    
+    private CombatAnimationData currentAnimation;
+    
+    private float currentAnimationNormalizeDuration;
     private float attack1AnimationTime;
     private float attack2AnimationTime;
 
     private float attackTimeLimit;
     private float nextComboBreak;
-
+    
     public PlayerAttackState(PlayerStateMachine stateMachine,PlayerMovement playerMovement) : base(stateMachine,playerMovement)
     {
     }
 
-    private CombatAnimationData currentAnimation;
-    private float currentAnimationNormalizeDuration;
     public override void Enter()
     {
         stateMachine.PlayerInput.OnDodgeInput += OnDodge;
@@ -24,27 +26,28 @@ public class PlayerAttackState : PlayerBaseState
         attackAnimationsDurations = new List<float>(new float[stateMachine.datas.animationClips.GetCombatAnimationData().Count]);
         SetAnimationDurationDatas();
         
-        
         attackTimeLimit = 0;
         nextComboBreak = 0;
         
         stateMachine.animator.CrossFadeInFixedTime(
             stateMachine.datas.animationClips.GetCurrentCombatAnimationName(stateMachine.combatData.CurrentCombatIndex),
-            0.5f);
+            0.5f); //TODO: We can change trasitionDuration???
         
+        //Get the current Animation combatData
         currentAnimation =
             stateMachine.datas.animationClips.GetCombatAnimationData()[stateMachine.combatData.CurrentCombatIndex];
+        //Get the current combat animation average duration diveded by our value. With this value we find kind of normalize animation duration
         currentAnimationNormalizeDuration =
             (currentAnimation.animation.averageDuration / currentAnimation.animationSpeed) - currentAnimation.animationDurationOffset;
         
-        
+        //Increase current combo count
         stateMachine.combatData.CurrentCombatIndex++;
         
+        //Check currentCombo is max or not
         if(stateMachine.combatData.CurrentCombatIndex >= stateMachine.comboDatas.AttackComboNamesList.Count)
         {
             stateMachine.combatData.CurrentCombatIndex = 0;
         }
-        
         
         playerMovement.AttackMovement(stateMachine.transform,0.4f,stateMachine.her);
     }
@@ -60,6 +63,8 @@ public class PlayerAttackState : PlayerBaseState
             {
                 stateMachine.SwitchState(new PlayerAttackState(stateMachine,playerMovement));
             }
+            //If there will be no combo input, change current animation to idle. The reason why is that not IdleState, so if current
+            // state chaged to idle there is no back to other lines.
             else
             {
                 stateMachine.animator.CrossFade(PlayerAnimationsNames.IdleAnim,0.1f);
@@ -73,7 +78,7 @@ public class PlayerAttackState : PlayerBaseState
             nextComboBreak += Time.deltaTime;
             
             //Player can continue his combo after a while later(currently 0.5)
-            if (nextComboBreak >= 0.5f)
+            if (nextComboBreak >= 0.5f) // TODO: We can change comboBreaker duration????
             {
                 
                 stateMachine.combatData.CurrentCombatIndex = 0;
